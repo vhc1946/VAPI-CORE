6bc445fb0a9d5d86f9680845852a118791f8f6f6
cvogel3434 <cvogel.a.m@gmail.com>
2022-11-01 21:49:16 -0500

    fix port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path'),
       http = require('http');
 var {exec} = require('child_process');
 
-var port = 8080; //port for local host
+var port = 5000; //port for local host
 
 var reqque=[];
 
